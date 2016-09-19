cf61e8aa11261ffb30731a7a6a4727e0dd044250
Unknown <jeuok@yahoo.fr>
2016-09-19 17:44:17 +0200

    Test CSS

--- rollsroyce.php
+++ rollsroyce.php
@@ -3,3 +3,8 @@
 include"header.php"
 
 ?>
+<section class="texte">
+    lore
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse itaque vero consectetur autem consequuntur voluptatum rem, illo in voluptate, ducimus modi. Ullam iusto nobis incidunt eius fugit error similique magni!
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas recusandae dolore, sequi sapiente facere officia rerum eveniet aliquid? Deleniti excepturi sapiente animi distinctio minus maiores, praesentium esse velit? Quaerat, labore.
+</section>
