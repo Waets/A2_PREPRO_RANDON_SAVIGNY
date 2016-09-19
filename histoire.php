15c8a68a9f35917587bb5b14e82381f7c96e1be1
Unknown <jeuok@yahoo.fr>
2016-09-19 18:31:45 +0200

    Add contenant on history page

--- histoire.php
+++ histoire.php
@@ -1,3 +1,11 @@
 <?php
 include"header.php";
 ?>
+
+    <section class="background"></section>
+        <section class="texte">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eligendi nobis enim ipsa amet architecto suscipit distinctio, qui maiores quod odit, tempore obcaecati natus dignissimos. Aspernatur modi iusto provident quo.
+
+
+        </section>
+    </section>
