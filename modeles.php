147c709097709d0cc3a362b60e6cbf64a5b7bcc0
Unknown <greenwaets@gmail.com>
2016-09-19 19:33:25 +0200

    Changed modele page

--- modeles.php
+++ modeles.php
@@ -5,20 +5,45 @@ include"header.php"
 ?>
 <section class="col-md-3">
 
-<img src="img/Phantom.jpg" alt="Smiley face" width="100%">PHANTOM Unmistakably Rolls-Royce
+<img src="img/Phantom.jpg" alt="Smiley face" width="80%"><br>PHANTOM Unmistakably Rolls-Royce <hr>
 
-<img src="img/Ghost.jpg" alt="Smiley face" width="100%">GHOST Effortless simplicity
+<img src="img/Ghost.jpg" alt="Smiley face" width="80%"><br>GHOST Effortless simplicity <hr>
 
-<img src="img/Wraith.jpg" alt="Smiley face" width="100%">WRAITH Power. Style. Drama
+<img src="img/Wraith.jpg" alt="Smiley face" width="80%"><br>WRAITH Power. Style. Drama <hr>
 
-<img src="img/Dawn.jpg" alt="Smiley face" width="100%">DAWN Strikingly seductive
+<img src="img/Dawn.jpg" alt="Smiley face" width="80%"><br>DAWN Strikingly seductive
 
 
 </section>
 
 
-<section class="col-xs-12 col-sm-6 col-md-8">
-	<h1>Les modèles :
+<section class="col-xs-12">
+	<h1>Les modèles : Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</h1>
 </section>
 
