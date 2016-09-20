2d6d18360de27fce113268377854868dd71794ad
Unknown <jeuok@yahoo.fr>
2016-09-21 00:50:37 +0200

    Add Component on history page

--- histoire.php
+++ histoire.php
@@ -4,8 +4,16 @@ include"header.php";
 
     <section class="background">
         <section class="texte">
+     <h1>Historique</h1>
+        
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eligendi nobis enim ipsa amet architecto suscipit distinctio, qui maiores quod odit, tempore obcaecati natus dignissimos. Aspernatur modi iusto provident quo.
-
-
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quidem veritatis impedit excepturi soluta accusantium tenetur odio consequatur laboriosam ex voluptatibus, alias repudiandae. Quam dolorem, quod saepe optio dolore atque!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A enim deleniti neque! Incidunt magnam voluptas voluptates saepe adipisci fuga perspiciatis ipsam eos quos quasi eaque eum, reiciendis ad voluptatem tempora.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas blanditiis dolore ex voluptate recusandae earum nemo, laborum. Officia id quos doloremque minima qui, esse suscipit natus a, tempore nobis, sed!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam, blanditiis vitae recusandae iste cum sit et culpa deserunt ab excepturi reprehenderit dolore vel velit minus? Quidem dolorum, reprehenderit laudantium.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quod officiis explicabo laboriosam! Aliquam accusantium impedit, eos distinctio beatae repudiandae facere tempore officiis consectetur, recusandae aspernatur natus architecto illo ipsum!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At saepe sed similique repudiandae quo doloremque ratione dolores blanditiis nemo qui asperiores architecto perferendis quisquam voluptatum corrupti, dignissimos, officiis adipisci tempora!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error voluptates aspernatur quo est consequatur architecto. Ab provident quam harum perspiciatis, excepturi aliquam, quas voluptatibus illo unde recusandae ullam saepe consequuntur!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque sed ad cupiditate ea sint, reprehenderit veniam soluta, maxime cum dolore. Ullam rem sunt, repudiandae eos, atque dolorum obcaecati quibusdam corporis.
         </section>
     </section>
