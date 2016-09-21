19a047ce07ee170b609aa97f8d7fd138d4928abf
Unknown <greenwaets@gmail.com>
2016-09-21 20:25:16 +0200

    added style on model.php

--- modeles.php
+++ modeles.php
@@ -70,5 +70,47 @@ include "header.php";
             </div>
 
         </div>
+           <div class="col-md-9 col-md-offset-3">
+            <div class="single-car">
+                <img src="img/phantomsalon.jpg" alt="Smiley face" width="1300" height="700">
+                <div class="overlay">
+                    <h2>Phantom</h2>
+                    <p>
+                        A commanding presence, Phantom Family encapsulates contemporary luxury. Its signature
+                        design is
+                        complemented by modern features, with the potential to personalise it to the smallest detail.
+                        Born
+                        of the
+                        desire to build The Best Car in the World, Phantom is the result of complete creative and
+                        engineering
+                        freedom.
+
+                        When it came to designing the current Phantom Family, it’s fitting that the influence came from
+                        Sir
+                        Henry
+                        Royce himself: “Strive for perfection in everything you do. Take the best that exists and make
+                        it
+                        better.
+                        When it doesn’t exist, design it.” From that challenge, Phantom was born. And the result is the
+                        definition
+                        of automotive design and technological perfection.
+
+                        Phantom is a timeless interpretation of the modern luxury motor car. With its powerful stance,
+                        iconic
+                        proportions and state-of-the-art technology, it’s the signature Rolls-Royce. The engine delivers
+                        an
+                        abundance of effortless power – a magic carpet-like ride – that creates a driving experience
+                        like no
+                        other.
+                        But the pleasure begins even before you own a Phantom, as you can choose the personal features
+                        to
+                        make your
+                        motor car truly unique. This is a motor car to be commissioned, to represent your personal
+                        expression.
+                    </p>
+                </div>
+            </div>
+
+        </div>
     </div>
 </div>
