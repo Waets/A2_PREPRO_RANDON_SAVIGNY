f8ed33ca4c7a8881c564ff3e074210f86be1da5e
Unknown <jeuok@yahoo.fr>
2016-09-21 18:10:11 +0200

    add new part on history page

--- histoire.php
+++ histoire.php
@@ -1,50 +1,24 @@
 <?php
 include"header.php";
 ?>
-
-    <section class="background">
-        <section class="texte">
-     <h1>Historique</h1>
-       <div id="img">
-<img src="img/history1.jpg" />
+	<link rel="stylesheet" href="style.css">
+	<section class="background">
+		<section class="texte">
+			<h1>Historique</h1>
+			<div id="img">
+				<img src="img/history1.jpg" />
 			</div>
-       <br><br>	
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eligendi nobis enim ipsa amet architecto suscipit distinctio, qui maiores quod odit, tempore obcaecati natus dignissimos. Aspernatur modi iusto provident quo.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quidem veritatis impedit excepturi soluta accusantium tenetur odio consequatur laboriosam ex voluptatibus, alias repudiandae. Quam dolorem, quod saepe optio dolore atque!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A enim deleniti neque! Incidunt magnam voluptas voluptates saepe adipisci fuga perspiciatis ipsam eos quos quasi eaque eum, reiciendis ad voluptatem tempora.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas blanditiis dolore ex voluptate recusandae earum nemo, laborum. Officia id quos doloremque minima qui, esse suscipit natus a, tempore nobis, sed!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam, blanditiis vitae recusandae iste cum sit et culpa deserunt ab excepturi reprehenderit dolore vel velit minus? Quidem dolorum, reprehenderit laudantium.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quod officiis explicabo laboriosam! Aliquam accusantium impedit, eos distinctio beatae repudiandae facere tempore officiis consectetur, recusandae aspernatur natus architecto illo ipsum!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. At saepe sed similique repudiandae quo doloremque ratione dolores blanditiis nemo qui asperiores architecto perferendis quisquam voluptatum corrupti, dignissimos, officiis adipisci tempora!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error voluptates aspernatur quo est consequatur architecto. Ab provident quam harum perspiciatis, excepturi aliquam, quas voluptatibus illo unde recusandae ullam saepe consequuntur!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque sed ad cupiditate ea sint, reprehenderit veniam soluta, maxime cum dolore. Ullam rem sunt, repudiandae eos, atque dolorum obcaecati quibusdam corporis.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam ut tempore neque voluptates laudantium sequi, inventore ratione modi officiis aperiam eligendi enim expedita reprehenderit laborum praesentium, quam porro a! Porro.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nisi nemo placeat natus, praesentium distinctio, expedita nobis quos quis consequatur ad! Hic obcaecati nemo corporis temporibus perferendis repudiandae, blanditiis beatae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolores impedit atque sint dolore vero, eaque voluptates sapiente vitae quo. Nulla eligendi voluptatem error, quo tenetur sit doloremque velit veritatis.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo atque dolore neque nesciunt, hic quis, animi assumenda voluptatem vero minima nemo itaque, reiciendis quas ab sequi vel reprehenderit perferendis eum!
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab praesentium quaerat ipsam tempore ducimus sapiente qui veniam officiis pariatur eaque, dignissimos voluptas ullam, molestiae fugiat nihil alias animi sunt at?
-                
-	
-        </section>
-        
-    </section>
-    <br>
-    <div class="first">
-    <section class="background1"> 
-            <section class="texte1">
-            
-			<div id="img1">
-		<img src="img/fondateur2.jpg"/>
-				<div id="img2">
-		<img src="img/fondateur1.jpg"/>
-					</div>
-					
-				</section>
-					</div>
-     		
-     		
-      		
-            
-            </section>
-    </section>
-    </div>
+			<br>
+			<br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eligendi nobis enim ipsa amet architecto suscipit distinctio, qui maiores quod odit, tempore obcaecati natus dignissimos. Aspernatur modi iusto provident quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quidem veritatis impedit excepturi soluta accusantium tenetur odio consequatur laboriosam ex voluptatibus, alias repudiandae. Quam dolorem, quod saepe optio dolore atque! Lorem ipsum dolor sit amet, consectetur adipisicing elit. A enim deleniti neque! Incidunt magnam voluptas voluptates saepe adipisci fuga perspiciatis ipsam eos quos quasi eaque eum, reiciendis ad voluptatem tempora. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas blanditiis dolore ex voluptate recusandae earum nemo, laborum. Officia id quos doloremque minima qui, esse suscipit natus a, tempore nobis, sed! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam, blanditiis vitae recusandae iste cum sit et culpa deserunt ab excepturi reprehenderit dolore vel velit minus? Quidem dolorum, reprehenderit laudantium. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quod officiis explicabo laboriosam! Aliquam accusantium impedit, eos distinctio beatae repudiandae facere tempore officiis consectetur, recusandae aspernatur natus architecto illo ipsum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. At saepe sed similique repudiandae quo doloremque ratione dolores blanditiis nemo qui asperiores architecto perferendis quisquam voluptatum corrupti, dignissimos, officiis adipisci tempora! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error voluptates aspernatur quo est consequatur architecto. Ab provident quam harum perspiciatis, excepturi aliquam, quas voluptatibus illo unde recusandae ullam saepe consequuntur! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque sed ad cupiditate ea sint, reprehenderit veniam soluta, maxime cum dolore. Ullam rem sunt, repudiandae eos, atque dolorum obcaecati quibusdam corporis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam ut tempore neque voluptates laudantium sequi, inventore ratione modi officiis aperiam eligendi enim expedita reprehenderit laborum praesentium, quam porro a! Porro. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel nisi nemo placeat natus, praesentium distinctio, expedita nobis quos quis consequatur ad! Hic obcaecati nemo corporis temporibus perferendis repudiandae, blanditiis beatae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque dolores impedit atque sint dolore vero, eaque voluptates sapiente vitae quo. Nulla eligendi voluptatem error, quo tenetur sit doloremque velit veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo atque dolore neque nesciunt, hic quis, animi assumenda voluptatem vero minima nemo itaque, reiciendis quas ab sequi vel reprehenderit perferendis eum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab praesentium quaerat ipsam tempore ducimus sapiente qui veniam officiis pariatur eaque, dignissimos voluptas ullam, molestiae fugiat nihil alias animi sunt at?
+
+
+		</section>
+
+	</section>
+	<br>
+	<div id="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit voluptas quo dignissimos ad natus beatae dolore est enim asperiores modi iure, maxime a, doloremque mollitia rem et quidem necessitatibus expedita.</div>
+	<div id="prout"><img src="img/fondateur1.jpg" alt=""></div>
+	<div id="bla"><img src="img/fondateur2.jpg" alt=""></div>
+	<div id="blabla">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et ratione fugit possimus quas libero, odio harum, in rerum nam corporis nobis laudantium reprehenderit. Nemo amet quae, magni sapiente esse deserunt.</div>
+
+	</div>
