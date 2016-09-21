a410f886a2dc50d895e5f186dd2fbd8b68e165df
Unknown <greenwaets@gmail.com>
2016-09-21 15:20:45 +0200

    Added text on phantom image and fixed some scss things

--- modeles.php
+++ modeles.php
@@ -19,34 +19,19 @@ include"header.php"
 
 
 <section class="col-xs-12">
-	<h1>Phantom</h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-	</h1>
+	<h1>Phantom</h1>
+
+	<div id="imagemodele"><img src="img/phantomsalon.jpg" alt="Smiley face" width="1300" height="700">
+	<h2 class="textesurimage">A commanding presence, Phantom Family encapsulates contemporary luxury. Its signature design is complemented by modern features, with the potential to personalise it to the smallest detail. Born of the desire to build The Best Car in the World, Phantom is the result of complete creative and engineering freedom.
+
+When it came to designing the current Phantom Family, it’s fitting that the influence came from Sir Henry Royce himself: “Strive for perfection in everything you do. Take the best that exists and make it better. When it doesn’t exist, design it.” From that challenge, Phantom was born. And the result is the definition of automotive design and technological perfection.
+
+Phantom is a timeless interpretation of the modern luxury motor car. With its powerful stance, iconic proportions and state-of-the-art technology, it’s the signature Rolls-Royce. The engine delivers an abundance of effortless power – a magic carpet-like ride – that creates a driving experience like no other. But the pleasure begins even before you own a Phantom, as you can choose the personal features to make your motor car truly unique. This is a motor car to be commissioned, to represent your personal expression.
+	</h2>
+	</div>
+
 </section>
+
 
 
 
