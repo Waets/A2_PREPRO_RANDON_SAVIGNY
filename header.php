0d965b04d715279b92c4fcb84d9af2ed18d189b3
JeremyBellaiche <contact@jeremybellaiche.com>
2016-09-21 19:01:03 +0200

    Fix by Jeremy

--- header.php
+++ header.php
@@ -20,8 +20,8 @@
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
 </head>
@@ -29,40 +29,40 @@
 <body>
 
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-        <center><a href="index.php"><img src="img/rollslogo.png" id="oui" width="60"></a></center>
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Home</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">                  
-                    <li>
-                        <a href="rollsroyce.php">Rolls Royce</a>
-                    </li>   
-                    <li>
-                        <a href="modeles.php">Models</a>
-                    </li>
-                    <li>
-                        <a href="histoire.php">Story</a>
-                    </li>         
-                </ul>
-            </div>          <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <a href="index.php">
+            <img src="img/rollslogo.png" class="logo" width="60">
+        </a>
 
-    <li>
-                    
-    </li>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Home</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="rollsroyce.php">Rolls Royce</a>
+                </li>
+                <li>
+                    <a href="modeles.php">Models</a>
+                </li>
+                <li>
+                    <a href="histoire.php">Story</a>
+                </li>
+            </ul>
+        </div>          <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
--- modeles.php
+++ modeles.php
@@ -1,73 +1,74 @@
 <?php
 
-include"header.php"
+include "header.php";
 
 ?>
 
-<section class="col-md-3">
+<hr>
+<hr>
 
-<a href="#one"><img src="img/Phantom.jpg" alt="Smiley face" width="80%"><br>PHANTOM Unmistakably Rolls-Royce</a> <hr>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3 cars-menu no-padding">
+            <a href="#one">
+                <img src="img/Phantom.jpg" alt="Smiley face" width="80%">
+                <span>PHANTOM Unmistakably Rolls-Royce</span>
+            </a>
+            <a href="#two">
+                <img src="img/Ghost.jpg" alt="Smiley face" width="80%">
+                <span>GHOST Effortless simplicity</span>
+            </a>
+            <a href="#three">
+                <img src="img/Wraith.jpg" alt="Smiley face" width="80%">
+                <span>WRAITH Power. Style. Drama</span>
+            </a>
+            <a href="#four">
+                <img src="img/Dawn.jpg" alt="Smiley face" width="80%">
+                <span>DAWN Strikingly seductive</span>
+            </a>
+        </div>
 
-<a href="#two"><img src="img/Ghost.jpg" alt="Smiley face" width="80%"><br>GHOST Effortless simplicity</a> <hr>
+        <div class="col-md-9 col-md-offset-3">
+            <div class="single-car">
+                <img src="img/phantomsalon.jpg" alt="Smiley face" width="1300" height="700">
+                <div class="overlay">
+                    <h2>Phantom</h2>
+                    <p>
+                        A commanding presence, Phantom Family encapsulates contemporary luxury. Its signature
+                        design is
+                        complemented by modern features, with the potential to personalise it to the smallest detail.
+                        Born
+                        of the
+                        desire to build The Best Car in the World, Phantom is the result of complete creative and
+                        engineering
+                        freedom.
 
-<a href="#three"><img src="img/Wraith.jpg" alt="Smiley face" width="80%"><br>WRAITH Power. Style. Drama</a> <hr>
+                        When it came to designing the current Phantom Family, it’s fitting that the influence came from
+                        Sir
+                        Henry
+                        Royce himself: “Strive for perfection in everything you do. Take the best that exists and make
+                        it
+                        better.
+                        When it doesn’t exist, design it.” From that challenge, Phantom was born. And the result is the
+                        definition
+                        of automotive design and technological perfection.
 
-<a href="#four"><img src="img/Dawn.jpg" alt="Smiley face" width="80%"><br>DAWN Strikingly seductive</a>
+                        Phantom is a timeless interpretation of the modern luxury motor car. With its powerful stance,
+                        iconic
+                        proportions and state-of-the-art technology, it’s the signature Rolls-Royce. The engine delivers
+                        an
+                        abundance of effortless power – a magic carpet-like ride – that creates a driving experience
+                        like no
+                        other.
+                        But the pleasure begins even before you own a Phantom, as you can choose the personal features
+                        to
+                        make your
+                        motor car truly unique. This is a motor car to be commissioned, to represent your personal
+                        expression.
+                    </p>
+                </div>
+            </div>
 
-
-</section>
-
-
-<section class="col-xs-12"><img src="img/clictonavigate.jpg" alt="Smiley face" >
-
-	<div id="imagemodele"><img src="img/phantomsalon.jpg" alt="Smiley face" width="1300" height="700">
-	<h2 class="textesurimage"><strong><center><a id="one">Phantom :</a></center></strong><br>A commanding presence, Phantom Family encapsulates contemporary luxury. Its signature design is complemented by modern features, with the potential to personalise it to the smallest detail. Born of the desire to build The Best Car in the World, Phantom is the result of complete creative and engineering freedom.
-
-When it came to designing the current Phantom Family, it’s fitting that the influence came from Sir Henry Royce himself: “Strive for perfection in everything you do. Take the best that exists and make it better. When it doesn’t exist, design it.” From that challenge, Phantom was born. And the result is the definition of automotive design and technological perfection.
-
-Phantom is a timeless interpretation of the modern luxury motor car. With its powerful stance, iconic proportions and state-of-the-art technology, it’s the signature Rolls-Royce. The engine delivers an abundance of effortless power – a magic carpet-like ride – that creates a driving experience like no other. But the pleasure begins even before you own a Phantom, as you can choose the personal features to make your motor car truly unique. This is a motor car to be commissioned, to represent your personal expression.
-	</h2>
-	</div>
-<br><br>	
-
-<div id="imagemodele2"><img src="img/ghostsalon.jpg" alt="Smiley face" width="1300" height="700">
-<h2 class="textesurimage2"><strong><center><a id="two">Ghost :</a></center></strong><br>
-Alive with cutting-edge technology, Ghost is one of the most advanced cars ever built. The essence of simplicity with signature levels of luxury, Ghost forges a silent path with effortless power. Which leaves only one question: drive or be driven?
-
-Extraordinary may be a high standard to set. But, in the case of Ghost, it is merely the benchmark. Advanced engineering and a host of hidden technologies make Ghost modern and dynamic. And its endless surge of power and whisper-quiet ride set it streets apart from other motor cars.
-Every detail is precisely engineered for an effortless driving experience: one that becomes intuitive as technology delivers more intelligence than ever before. But Ghost is about more than what lies beneath the bonnet.
-The design at the front brings poise and verve, while the luxurious interior – with soft, supple leather and indulgent lambswool floor mats – cossets you from the outside world.
-
-Uncompromising, never ordinary, Ghost offers an effortless and immersive experience.
-
-
-</h2>
+        </div>
+    </div>
 </div>
-
-<br>	
-
-<div id="imagemodele3"><img src="img/wraithsalon.jpg" alt="Smiley face" width="1300" height="700">
-<h2 class="textesurimage3"><strong><center><a id="three">Wraith :</a></center></strong><br>
-Wraith was born of a desire to push the boundaries of design and engineering. Dynamic lines create an enigmatic presence - a presence which plays host to the most powerful Rolls-Royce engine ever. Unashamedly built to deliver, Wraith challenges perceptions and offers the most dynamic driving experience in the marque’s history. 
-
-The name alone evokes a sense of noir. And the striking roofline gives a dynamic silhouette that enhances its power, style and drama. Inspired by European sporting and GT motor cars of the 1930s, the fastback design perfectly captures a spirit of adventure. The sweeping lines evoke a sense of movement, even while standing still. And the fascia is not only an opportunity to personalise: it creates an arrow-like energy – emphasising Wraith’s dynamic quality.
-
-Continuing the style, the largest coach doors in the Rolls-Royce family lead you into a contemporary cocoon, encasing you in an elegant simplicity Rolls-Royce is renowned for. With all eyes forward, Wraith’s dynamic lines and seamless surge of power redefine history. 
-
-
-</h2>
-</div>
-<br>
-<div id="imagemodele4"><img src="img/dawnsalon.jpg" alt="Smiley face" width="1300" height="700">
-<h2 class="textesurimage4"><strong><center><a id="four">Dawn :</a></center></strong><br>
-A Rolls-Royce like no other
-
-After the darkness comes Dawn. Its striking silhouette transformed at a touch, its inviting interior opened to the elements – to friends, to the unexpected, to endless possibilities. Dawn lives in the moment. The moment is now.
-
-
-</h2>
-</div>
-</section>
-
-
-
