c487ff02061eb8631161645a63bca2ba5713aa7c
Unknown <greenwaets@gmail.com>
2016-09-23 16:12:30 +0200

    Changed modeles, bg and gallery

--- modeles.php
+++ modeles.php
@@ -72,45 +72,53 @@ include "header.php";
         </div>
            <div class="col-md-9 col-md-offset-3">
             <div class="single-car">
-                <img src="img/phantomsalon.jpg" alt="Smiley face" width="100%" height="700">
+                <img src="img/ghostsalon.jpg" alt="Smiley face" width="100%" height="700">
                 <div class="overlay">
-                    <h2>Phantom</h2>
+                    <h2>Ghost</h2>
                     <p>
-                        A commanding presence, Phantom Family encapsulates contemporary luxury. Its signature
-                        design is
-                        complemented by modern features, with the potential to personalise it to the smallest detail.
-                        Born
-                        of the
-                        desire to build The Best Car in the World, Phantom is the result of complete creative and
-                        engineering
-                        freedom.
+                        Alive with cutting-edge technology, Ghost is one of the most advanced cars ever built. The essence of simplicity with signature levels of luxury, Ghost forges a silent path with effortless power. Which leaves only one question: drive or be driven?
 
-                        When it came to designing the current Phantom Family, it’s fitting that the influence came from
-                        Sir
-                        Henry
-                        Royce himself: “Strive for perfection in everything you do. Take the best that exists and make
-                        it
-                        better.
-                        When it doesn’t exist, design it.” From that challenge, Phantom was born. And the result is the
-                        definition
-                        of automotive design and technological perfection.
+Extraordinary may be a high standard to set. But, in the case of Ghost, it is merely the benchmark. Advanced engineering and a host of hidden technologies make Ghost modern and dynamic. And its endless surge of power and whisper-quiet ride set it streets apart from other motor cars.
+Every detail is precisely engineered for an effortless driving experience: one that becomes intuitive as technology delivers more intelligence than ever before. But Ghost is about more than what lies beneath the bonnet.
+The design at the front brings poise and verve, while the luxurious interior – with soft, supple leather and indulgent lambswool floor mats – cossets you from the outside world.
 
-                        Phantom is a timeless interpretation of the modern luxury motor car. With its powerful stance,
-                        iconic
-                        proportions and state-of-the-art technology, it’s the signature Rolls-Royce. The engine delivers
-                        an
-                        abundance of effortless power – a magic carpet-like ride – that creates a driving experience
-                        like no
-                        other.
-                        But the pleasure begins even before you own a Phantom, as you can choose the personal features
-                        to
-                        make your
-                        motor car truly unique. This is a motor car to be commissioned, to represent your personal
-                        expression.
+Uncompromising, never ordinary, Ghost offers an effortless and immersive experience.
                     </p>
                 </div>
             </div>
 
         </div>
+        <div class="col-md-9 col-md-offset-3">
+            <div class="single-car">
+                <img src="img/wraithsalon.jpg" alt="Smiley face" width="100%" height="700">
+                <div class="overlay">
+                    <h2>Wraith</h2>
+                    <p>
+                        Wraith was born of a desire to push the boundaries of design and engineering. Dynamic lines create an enigmatic presence - a presence which plays host to the most powerful Rolls-Royce engine ever. Unashamedly built to deliver, Wraith challenges perceptions and offers the most dynamic driving experience in the marque’s history. 
+
+The name alone evokes a sense of noir. And the striking roofline gives a dynamic silhouette that enhances its power, style and drama. Inspired by European sporting and GT motor cars of the 1930s, the fastback design perfectly captures a spirit of adventure. The sweeping lines evoke a sense of movement, even while standing still. And the fascia is not only an opportunity to personalise: it creates an arrow-like energy – emphasising Wraith’s dynamic quality.
+
+Continuing the style, the largest coach doors in the Rolls-Royce family lead you into a contemporary cocoon, encasing you in an elegant simplicity Rolls-Royce is renowned for. With all eyes forward, Wraith’s dynamic lines and seamless surge of power redefine history. 
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-md-9 col-md-offset-3">
+            <div class="single-car">
+                <img src="img/dawnsalon.jpg" alt="Smiley face" width="100%" height="700">
+                <div class="overlay">
+                    <h2>Dawn</h2>
+                    <p>
+                       After the darkness comes Dawn. Its striking silhouette transformed at a touch, its inviting interior opened to the elements – to friends, to the unexpected, to endless possibilities. Dawn lives in the moment. The moment is now.
+                       Dawn’s alluring presence is unmistakable, right from the very first encounter. Striking and assertive, its charm and charisma draw you in. With the top up, the mood is powerful. With one touch, its sensuous appeal is unveiled: the roof silently folds away, revealing a seductive interior.
+
+Discover a shape that’s unmistakably Rolls-Royce, whether the roof is up, down, or in motion. Classic design principles are coupled with the latest technology. Dawn’s dynamic angles and sleek surface create an elegant impression. The contemporary interior is crafted in anticipation of unforgettable moments between friends: a true four-seater, Dawn is an experience designed to be shared.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 </div>
